7e111d1e5a271a5e15dd704a70a000530d6aeeae
Hana Karbanová <hana@karbanova.cz>
2016-11-19 18:48:35 +0100

    second commit """ short version"""

--- anagram.py
+++ anagram.py
@@ -1,16 +1,11 @@
 dictionary = open('words.txt','r')
-word = 'elvis'
+word = 'lost'
 word_sort=''.join(sorted(word))
-
 l = len(word)
 seznam=[]
-
 for line in dictionary:
-    if l == len(line.strip()):
-        seznam.append((line.strip(),''.join(sorted(line.strip()))))
-
-anagram=[]
-for i in seznam:
-    if word_sort == i[1] and word != i[0]:
-        anagram.append(i[0])
-print(anagram)
+    s=line.strip()
+    if l == len(s):
+        if word_sort == ''.join(sorted(s)) and word !=s:
+            seznam.append(s)
+print(seznam)
