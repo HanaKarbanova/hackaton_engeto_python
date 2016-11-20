9e0872fc3b01c3a3639f4c7d665eca3b26d621d0
Hana Karbanová <hana@karbanova.cz>
2016-11-20 14:52:42 +0100

    third commit """more try to guess"""

--- hangman.py
+++ hangman.py
@@ -14,19 +14,20 @@ word_tuple=[]
 for i in word:
     word_tuple.append((i,0))
 '''how long player can guess'''
-repeat=len(word)
+lon=len(word)
 '''gues word'''
-print('We guess word about',repeat, 'letters')
+repeat=lon+5
+print('The word is',lon, 'letters long. You have', repeat, 'tries. Good luck.')
 letter = input("Guess letter:")
-while repeat >=1:
+while repeat >1:
     if letter in word:
         l=0
         for i in word_tuple:
             if letter==i[0] and i[1]==0:
                 word_tuple[l]=(i[0],1)
-                answer="You guest"
+                answer="You guess. You have tries to gues {}".format(repeat-1)
             elif letter==i[0] and i[1]==1:
-                answer='This letter is guessed!'
+                answer='This letter is guessed! You have tries to gues {}'.format(repeat-1)
             if word_tuple[l]==(i[0],1):
                 print(i[0],end="")
             else:
@@ -35,8 +36,7 @@ while repeat >=1:
         print()
         print(answer)
     else:
-        print("You didn't guess!")
+        print("You didn't guess! You have tries to gues",repeat-1)
     letter=input('Guess again:')
     repeat-=1
-
-print('You fail!')
+print('You fail! Word was >', ''.join(word))
